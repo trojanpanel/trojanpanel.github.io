288e82763c7863b1d56269d278237e7066cd4dd7
jonssonyan <yz808@outlook.com>
2023-03-29 15:16:18 +0800

    fix: update doc

--- vpress/.vuepress/config.js
+++ vpress/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    head: [['link', {rel: 'icon', href: 'logo.png'}]],
+    head: [['link', {rel: 'icon', href: 'logo.png'}], ['script', {}, `var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?37275f991707c27db91dbb6f668ee1c6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();`]],
     title: 'Trojan Panel',
     description: '支持Xray/Trojan-Go/Hysteria/NaiveProxy的多用户Web管理面板',
     plugins: [['@vuepress/back-to-top'], ['vuepress-plugin-code-copy', true]],
