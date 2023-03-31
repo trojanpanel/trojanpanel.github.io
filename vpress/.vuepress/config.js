dcbbd34137d0bb54ee3e2209b02e5cadf124e307
jonssonyan <yz808@outlook.com>
2023-03-31 11:35:40 +0800

    fix: update doc

--- vpress/.vuepress/config.js
+++ vpress/.vuepress/config.js
@@ -12,16 +12,29 @@ module.exports = {
     description: '支持Xray/Trojan-Go/Hysteria/NaiveProxy的多用户Web管理面板',
     plugins: [['@vuepress/back-to-top'], ['vuepress-plugin-code-copy', true]],
     themeConfig: {
-        logo: '/logo.png', lastUpdated: 'Last Updated', sidebar: {
-            '/start/': ['introduce', 'system-structure', 'related-video'],
-            '/tutorial/': ['using-tutorials', 'installation', 'custom-installation', 'docker-compose.md', 'des-of-related-doc', 'using-cdn', 'client-config', 'recommend-tool', 'performance-tuning', 'performance-testing'],
-            '/problem/': ['common-problem'],
-            '/change/': ['change-log']
-        }, nav: [{text: '新手起步', link: '/start/introduce'}, {text: '相关教程', link: '/tutorial/using-tutorials'}, {
+        logo: '/logo.png', lastUpdated: 'Last Updated', nav: [{text: '新手起步', link: '/start/introduce'}, {
+            text: '相关教程', items: [{
+                text: '安装教程', link: '/install-tutorial/installation'
+            }, {
+                text: '使用教程', link: '/tutorial/using-tutorials'
+            }]
+        }, {
             text: '常见问题', link: '/problem/common-problem'
+        }, {
+            text: '支持我们', items: [{
+                text: '增值服务', link: '/support/our-service'
+            }, {
+                text: '赞助', link: '/support/sponsor'
+            }]
         }, {text: '更新日志', link: '/change/change-log'}, {
             text: 'GitHub', link: 'https://github.com/trojanpanel'
-        }],
+        }], sidebar: {
+            '/start/': ['introduce', 'system-structure', 'related-video'],
+            '/tutorial/': ['using-tutorials', 'des-of-related-doc', 'using-cdn', 'client-config', 'recommend-tool', 'performance-tuning', 'performance-testing'],
+            '/install-tutorial/': ['installation', 'custom-installation', 'docker-compose'],
+            '/problem/': ['common-problem'],
+            '/change/': ['change-log']
+        },
     },
     dest: 'docs'
 }
---
+++ vpress/support/our-service.md
@@ -0,0 +1,9 @@
+# 增值服务
+
+你可以通过以下几种增值方式来支持Trojan Panel
+
+1. 数据迁移
+2. 集群搭建
+3. 技术运维/故障排查
+
+联系我们：[@jonssonyan](https://t.me/jonssonyan)
