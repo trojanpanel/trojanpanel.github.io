910dbafa2c8dda16c1dfacca47f111a3a2b705b8
jonssonyan <yz808@outlook.com>
2023-04-11 22:53:32 +0800

    fix: update doc

--- vpress/.vuepress/config.js
+++ vpress/.vuepress/config.js
@@ -20,12 +20,6 @@ module.exports = {
             }]
         }, {
             text: '常见问题', link: '/problem/common-problem'
-        }, {
-            text: '支持我们', items: [{
-                text: '增值服务', link: '/support/our-service'
-            }, {
-                text: '赞助', link: '/support/sponsor'
-            }]
         }, {text: '更新日志', link: '/change/change-log'}, {
             text: 'GitHub', link: 'https://github.com/trojanpanel'
         }], sidebar: {
